8aef2a391f002b9deac9bf8747373c61237d4f27
Shubham Yadav <41867232+shamy06@users.noreply.github.com>
2024-01-03 18:44:13 +0530

    Carousel

---
+++ Carousel/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ApiDetails from './Component/ApiDetails';
+
+function App() {
+  return (
+    <>
+    <ApiDetails/>
+    </>
+  );
+}
+
+export default App;

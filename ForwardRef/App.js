9a5eb8fd28704ab0f1ad505c9f2ed66c589fdaf1
Shubham Yadav <41867232+shamy06@users.noreply.github.com>
2024-07-10 21:23:12 +0530

    ForwardRef

---
+++ ForwardRef/App.js
@@ -0,0 +1,19 @@
+import { useRef } from 'react';
+import MyInput from './MyInput.js';
+
+export default function Form() {
+  const ref = useRef(null);
+
+  const handleClick=()=>{
+    ref.current.focus();
+  }
+
+  return(
+    <form>
+        <MyInput label="Enter your name:" ref={ref}/>
+        <button type="button" onClick={handleClick}>
+            Edit
+        </button>
+    </form>
+  );
+}
